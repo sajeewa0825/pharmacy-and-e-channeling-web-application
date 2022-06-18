4911e5f645d627b56d2cac584351a2a142e7da04
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-06-18 22:32:34 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 // import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import DoctorPage from "./DoctorPage";
+// import DoctorPage from "./DoctorPage";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./component/NavBar/nav";
+import Footer from "./component/Footer/footer";
 
 function App() {
   return (
     <div>
-      <DoctorPage/>
+      {/* <DoctorPage/> */}
+      <NavBar />
+      <Footer />
     </div>
   );
 }
