21e84dee9717c2d7fffe89cc6a44ae0b1f92c59d
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-06-23 19:05:46 +0530

    Update Landing.js

--- client/src/Landing/Landing.js
+++ client/src/Landing/Landing.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-    return (
-        <div>
-            <Link className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700" to={"/drhome"}>
-                Channeling
+  return (
+    <div>
+      <div className="container ">
+        <div className="row ">
+          <div className="col-md-6 d-flex justify-content-center">
+            <Link
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+              to={"/drhome"}
+            >
+              Channeling
             </Link>
-            <Link className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700" to={"/Phhome"}>
-                Pharmacy
+          </div>
+          <div className="col-md-6 d-flex justify-content-center">
+            <Link
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+              to={"/Phhome"}
+            >
+              Pharmacy
             </Link>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
