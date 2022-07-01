4863b65f8ca997972c8ab76166d902e6297fc89f
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-07-02 01:47:15 +0530

    Update AdminDashboard.js

--- client/src/AdminDashBoard/AdminDashboard.js
+++ client/src/AdminDashBoard/AdminDashboard.js
@@ -1,7 +1,75 @@
 import React from "react";
+import User from "./Images/icons8-user-100.png";
+import "./AdminDashboard.css";
+import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
-  return <div>AdminDashboard</div>;
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-2 adminLeft_col">
+            <div className="sidemenu_top">
+              <div className="sidemenu_top_User_img">
+                <span>Logo</span>
+                <hr />
+                <img src={User} alt="img" />
+                <span>Admin</span>
+              </div>
+            </div>
+            <div className="sidemenu_center">
+              <ul className="sidemenu_center_ul">
+                <li>
+                  <Link to={"./#"} className="sidemenu_center_ul_Link">
+                    <i class="bi bi-columns-gap"></i>
+                    <span>Dashboard</span>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to={"./#"} className="sidemenu_center_ul_Link">
+                    <i class="bi bi-heart-pulse"></i>
+                    <span>Doctor</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"./#"} className="sidemenu_center_ul_Link">
+                    <i class="bi bi-people"></i>
+                    <span>Patient</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"./#"} className="sidemenu_center_ul_Link">
+                    <i class="bi bi-card-heading"></i>
+                    <span>Appoinments</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"./#"} className="sidemenu_center_ul_Link">
+                    <i class="bi bi-bag-plus"></i>
+                    <span>Products</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"./#"} className="sidemenu_center_ul_Link">
+                    <i class="bi bi-credit-card-2-back"></i>
+                    <span>Payments</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"./#"} className="sidemenu_center_ul_Link">
+                    <i class="bi bi-truck"></i>
+                    <span>Delivery</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-10 adminRight_col"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
