82c61b4b4984443ff5abbbdae84c3af09cae643f
sajeewa0825 <sajeewa0825@gmail.com>
2022-07-01 20:26:51 +0530

    fix error

--- client/src/component/Products/FeatureProducts.js
+++ client/src/component/Products/FeatureProducts.js
@@ -4,7 +4,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../Products/FeaturesProducts.css";
-import { data } from 'jquery';
 
 const FeatureProduct = () => {
   var settings = {
@@ -63,7 +62,7 @@ const FeatureProduct = () => {
         <div class="card ">
           <div class="card-body">
             <h5 class="card-title">{data.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <h6 class="card-subtitle mb-2 text-muted">{data.price}</h6>
             <p class="card-text">
               Some quick example text to build on the card title and make up
               the bulk of the card's content.
