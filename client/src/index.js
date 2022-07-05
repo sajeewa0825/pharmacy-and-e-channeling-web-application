c45c98c073a3475f54eb6cd0ad7ebb7430263fba
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-07-06 02:00:14 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
-// import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 // import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import { ContextProvider } from './DoctorPage/component/Video/Context';
+import { ContextProvider } from "./DoctorPage/component/Video/Context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
