5a74d07d193b578230a7d1a8dfe8c02107b61b4c
Chalani Hansika <ict20068@std.uwu.ac.lk>
2022-07-16 22:18:46 +0530

    Update index.js

--- client/src/Routing/index.js
+++ client/src/Routing/index.js
@@ -1,42 +1,41 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homedr from '../DoctorPage/component/Home';
-import Login from '../DoctorPage/component/Login/Login';
-import SignUp from '../DoctorPage/component/Login/SignUp';
-import Head from '../DoctorPage/component/Navbar/Navhead';
-import Footer from '../component/Footer/footer';
-import Appoiment from '../DoctorPage/component/Appointment';
-import Landing from '../Landing/Landing';
-import Homeph from "../component/Home/home"
-import About from '../component/About/About';
-import Price from '../component/Price/price';
-import Shop from '../component/shop/shop';
-import Frogetpassword from '../DoctorPage/component/Login/Frogetpassword';
-import PasswordReset from '../DoctorPage/component/Login/ResetPassword';
-
-
+import Homedr from "../DoctorPage/component/Home";
+import Login from "../DoctorPage/component/Login/Login";
+import SignUp from "../DoctorPage/component/Login/SignUp";
+import Head from "../DoctorPage/component/Navbar/Navhead";
+import Footer from "../component/Footer/footer";
+import Appoiment from "../DoctorPage/component/Appointment";
+import Landing from "../Landing/Landing";
+import Homeph from "../component/Home/home";
+import About from "../component/About/About";
+import Price from "../component/Price/price";
+import Shop from "../component/shop/shop";
+import Frogetpassword from "../DoctorPage/component/Login/Frogetpassword";
+import PasswordReset from "../DoctorPage/component/Login/ResetPassword";
+import ContactUs from "../component/Contactus/ContactUs";
 
 const Routing = () => {
-
-    return (
-        <BrowserRouter>
-            <Head />
-            <Routes>
-                <Route path='signup' element={<SignUp />} />
-                <Route path='signin' element={<Login />} />
-                <Route path='appointment' element={<Appoiment />} />
-                <Route path='drhome' element={<Homedr />} />
-                <Route path='phhome' element={<Homeph />} />
-                <Route path='phaboutus' element={<About />} />
-                <Route path='phprice' element={<Price />} />
-                <Route path='phshop' element={<Shop />} />
-                <Route path='frogetpassword' element={<Frogetpassword />} />
-                <Route path='resetpassword' element={<PasswordReset/>} />
-                <Route path='/' element={<Landing />} />
-            </Routes>
-            <Footer/>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Head />
+      <Routes>
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<Login />} />
+        <Route path="appointment" element={<Appoiment />} />
+        <Route path="drhome" element={<Homedr />} />
+        <Route path="phhome" element={<Homeph />} />
+        <Route path="phaboutus" element={<About />} />
+        <Route path="phprice" element={<Price />} />
+        <Route path="phshop" element={<Shop />} />
+        <Route path="contact" element={<ContactUs />} />
+        <Route path="frogetpassword" element={<Frogetpassword />} />
+        <Route path="resetpassword" element={<PasswordReset />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default Routing;
