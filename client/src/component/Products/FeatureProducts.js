23ba053c7e6384567508d7628888e57d5fdb81a0
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-07-21 02:14:47 +0530

    Update FeatureProducts.js

--- client/src/component/Products/FeatureProducts.js
+++ client/src/component/Products/FeatureProducts.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -10,7 +10,7 @@ const FeatureProduct = () => {
     dots: false,
     infinite: true,
     speed: 400,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 2,
     initialSlide: 0,
     autoplay: true,
@@ -25,11 +25,21 @@ const FeatureProduct = () => {
         },
       },
       {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          infinite: true,
+        },
+      },
+      {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
           initialSlide: 2,
+          infinite: true,
         },
       },
       {
@@ -37,55 +47,53 @@ const FeatureProduct = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          infinite: true,
         },
       },
     ],
   };
 
-  const [Product, SetProduct] = useState([])
+  const [Product, SetProduct] = useState([]);
 
   useEffect(() => {
     const getproduct = () => {
-      axios.get("http://Localhost:8080/addproduct").then((res) => {
-        console.log(res.data)
-        SetProduct(res.data)
-      }).catch((err) => {
-        alert(err)
-      })
-    }
+      axios
+        .get("http://Localhost:8080/addproduct")
+        .then((res) => {
+          console.log(res.data);
+          SetProduct(res.data);
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    };
     getproduct();
-  }, [])
+  }, []);
 
   let ProductData = Product.map((data) => {
     return (
       <div>
         <div class="card ">
-          <img
-            src={data.imgLink}
-            class="card-img-top"
-            alt="img"
-          />
+          <img src={data.imgLink} class="card-img-top" alt="img" />
 
           <div class="card-body">
             <h5 class="card-title">{data.name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{data.price}</h6>
             <p class="card-text">
-              Some quick example text to build on the card title and make up
-              the bulk of the card's content.
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </p>
           </div>
         </div>
       </div>
-    )
-  })
+    );
+  });
   return (
     <div className="FPSection">
       <h2 class="text-center"> Featurs Products </h2>
-      <Slider {...settings}>
-        {ProductData}
-      </Slider>
+      <Slider {...settings}>{ProductData}</Slider>
     </div>
   );
-}
+};
 
 export default FeatureProduct;
