233cf26056391f94ab7a322e85ad1c40722908c3
sajeewa0825 <sajeewa0825@gmail.com>
2022-07-21 03:34:27 +0530

    create doctor nav

--- client/src/DoctorPage/component/Navbar/Navbar.js
+++ client/src/DoctorPage/component/Navbar/Navbar.js
@@ -18,40 +18,7 @@ import {
 } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 
-const solutions = [
-  {
-    name: "Analytics",
-    description:
-      "Get a better understanding of where your traffic is coming from.",
-    href: "#",
-    icon: ChartBarIcon,
-  },
-  {
-    name: "Engagement",
-    description: "Speak directly to your customers in a more meaningful way.",
-    href: "#",
-    icon: CursorClickIcon,
-  },
-  {
-    name: "Security",
-    description: "Your customers' data will be safe and secure.",
-    href: "#",
-    icon: ShieldCheckIcon,
-  },
-  {
-    name: "Integrations",
-    description: "Connect with third-party tools that you're already using.",
-    href: "#",
-    icon: ViewGridIcon,
-  },
-  {
-    name: "Automations",
-    description:
-      "Build strategic funnels that will drive your customers to convert",
-    href: "#",
-    icon: RefreshIcon,
-  },
-];
+
 const callsToAction = [
   { name: "Watch Demo", href: "#", icon: PlayIcon },
   { name: "Contact Sales", href: "#", icon: PhoneIcon },
@@ -179,14 +146,7 @@ export default function NavBar() {
                         "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                       )}
                     >
-                      <span className="setfont">More</span>
-                      <ChevronDownIcon
-                        className={classNames(
-                          open ? "text-gray-600" : "text-gray-400",
-                          "ml-2 h-5 w-5 group-hover:text-gray-500"
-                        )}
-                        aria-hidden="true"
-                      />
+
                     </Popover.Button>
 
                     <Transition
@@ -201,26 +161,7 @@ export default function NavBar() {
                       <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
                         <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                           <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                            {solutions.map((item) => (
-                              <a
-                                key={item.name}
-                                href={item.href}
-                                className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50 setfont"
-                              >
-                                <item.icon
-                                  className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                                  aria-hidden="true"
-                                />
-                                <div className="ml-4">
-                                  <p className="text-base font-medium text-gray-900 setfont">
-                                    {item.name}
-                                  </p>
-                                  <p className="mt-1 text-sm text-gray-500 setfont">
-                                    {item.description}
-                                  </p>
-                                </div>
-                              </a>
-                            ))}
+
                           </div>
 
                           <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
@@ -282,21 +223,13 @@ export default function NavBar() {
                 </div>
                 <div className="mt-6">
                   <nav className="grid gap-y-8">
-                    {solutions.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 setfont"
-                      >
-                        <item.icon
-                          className="flex-shrink-0 h-6 w-6 text-indigo-600 setfont"
-                          aria-hidden="true"
-                        />
-                        <span className="ml-3 text-base font-medium text-gray-900 setfont">
-                          {item.name}
-                        </span>
-                      </a>
-                    ))}
+                    <Link
+                      className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 setfont"
+                      to={"/phhome"}
+                    >
+                      pharmacy
+                    </Link>
+
                   </nav>
                 </div>
               </div>
